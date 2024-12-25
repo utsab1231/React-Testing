69e94e90a0439ba6bf4c12b4673fbed1207d5835
Utsab1231 <utssabad10@gmail.com>
2024-12-25 07:49:59 +0545

    Added method to group the test together

--- jest-testing/src/components/Greetings/Greet.test.tsx
+++ jest-testing/src/components/Greetings/Greet.test.tsx
@@ -2,15 +2,17 @@ import { render, screen } from "@testing-library/react";
 import Greet from "./Greet";
 // Greet component should render the text "hello" and if a name prop is passed, it should render "hello <name>".
 
-test("Greet component should render the text 'hello'", () => {
-  render(<Greet />);
-  const element = screen.getByText(/hello/i);
-  expect(element).toBeInTheDocument();
-});
+describe("Greet Component", () => {
+  test("renders the text 'hello'", () => {
+    render(<Greet />);
+    const element = screen.getByText(/hello/i);
+    expect(element).toBeInTheDocument();
+  });
 
-test("Greet component should render the text 'hello <name>' if name is provided", () => {
+  test("renders the text 'hello <name>' if name is provided", () => {
     let name;
     render(<Greet name={name} />);
     const element = screen.getByText(/hello/i);
     expect(element).toBeInTheDocument();
+  });
 });
